a4316bc4619ba9b2b21ef29eca2eadea0790c167
Luca Ford <lucasubiranaford@gmail.com>
2019-07-26 23:48:36 -0300

    Change files to mayusc

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 
-import Loading from './src/screens/loading'
-import Register from './src/screens/register'
-import Login from './src/screens/login'
-import Home from './src/screens/home'
+import Loading from './src/screens/Loading'
+import Register from './src/screens/Register'
+import Login from './src/screens/Login'
+import Home from './src/screens/Home'
 
 const MainNavigator = createStackNavigator({
   Login: {screen: Login},
